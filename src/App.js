b90e7583d17f5907d2607c1efc263b2c530773b4
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-05-10 12:55:35 +0300

    Handling Loading & Data States &  Http Errors

--- src/App.js
+++ src/App.js
@@ -4,32 +4,50 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function tetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => {
-        return response.json();
-      }).then(data => {
-        const transformedMovies = data.results.map(movies => {
-          return {
-            id: movies.episode_id,
-            title: movies.title,
-            openingText: movies.opening_crawl,
-            releaseDate: movies.release_date
-          }
-        })
-        setMovies(transformedMovies)
-      })
+
+
+  async function tetchMoviesHandler() {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+
+      if (!response.ok) throw new Error("Somethign went wrong!");
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movies => {
+        return {
+          id: movies.episode_id,
+          title: movies.title,
+          openingText: movies.opening_crawl,
+          releaseDate: movies.release_date
+        }
+      });
+
+      setMovies(transformedMovies);
+
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={tetchMoviesHandler} >Fetch Movies</button>
+        <button onClick={tetchMoviesHandler} disabled={loading}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!loading && movies.length === 0 && !error && <p>Failed no movies!</p>}
+        {!loading && error && <p>{error}</p>}
+        {loading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
